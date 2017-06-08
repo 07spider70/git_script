f690a234c0ce3261115a4c590f9cc925ec83ed3b
Jan Remen <jan.remen98@gmail.com>
2017-06-08 20:56:13 +0200

    Adding comments

--- lib.py
+++ lib.py
@@ -2,32 +2,32 @@
 import os
 import json
 
-def open_json(data_f='conf.json'):
+def open_json(data_f='conf.json'): #open json config file
     with open(data_f) as data_file:
         data = json.load(data_file)
     return(data)
 
-def cd(data):
+def cd(data):   #change directory according to config file
     #call(['cd',path])
     path = data['config']['patch']
     os.system('cd '+ path)
     return()
 
-def mess():
+def mess(): #set message for commit
     message = (str(input('Zadaj text: ')))
     return message
 
-def init():
+def init(): #inicialize git folder
     #call(['git','init'])
     os.system('git init')
     return()
 
-def add():
+def add():  #add all from folder to git
     #call(['git','add','.'])
     os.system('git add .')
     return()
 
-def config(data):
+def config(data):   #config for git according to config file
     #data = open_json()
     name = "'"+ data['config']['name'] +"'"
     email = data['config']['email']
@@ -39,11 +39,11 @@ def config(data):
     print('Added ' + email)
     return()
 
-def commit(message):
+def commit(message):    #commit folder to git
     #cal(['git','commit','-m',message])
     os.system('git commit -m '+ '"' + str(message) + '"')
     return()
 
-def push():
+def push(): #upload folder to git repository
     os.system('git push')
     return()
